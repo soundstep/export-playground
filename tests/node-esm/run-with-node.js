cdb667e05cad7e9e5d8ee530bdf41016ec8979e8
Romuald Quantin <romu@soundstep.com>
2022-01-26 14:42:49 +0000

    Added nextjs4.

---
+++ tests/node-esm/run-with-node.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+// level 0
+// import lib from 'export-playground';
+// import { info } from 'export-playground';
+import { login, logout } from 'export-playground/auth';
+// import { commercial, sponsorship } from 'export-playground/deep/ads/index.js';
+
+// console.log('-- 0', lib);
+
+// level 1
+// info();
+
+// level 2
+login();
+logout();
+
+// level 3
+// commercial();
+// sponsorship();
